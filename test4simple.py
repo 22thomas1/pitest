fe8e0a98f55d8af19eb9fcd711ac21182e66423d
22thomas1 <40281620+22thomas1@users.noreply.github.com>
2021-04-24 16:25:13 -0500

    Update test4simple.py

--- test4simple.py
+++ test4simple.py
@@ -2,7 +2,7 @@ from  pycreate2 import Create2
 import time
 
 # Create a Create2.
-port = "/dev/serial"  # where is your serial port?
+port = "/dev/ttyAMA0"  # where is your serial port?
 bot = Create2(port)
 
 # Start the Create 2
