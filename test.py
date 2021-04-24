cad5d164526c56ad92f27a9289c210a5a2ff6dee
22thomas1 <40281620+22thomas1@users.noreply.github.com>
2021-04-24 07:53:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,8 @@ from time import sleep
 
 red=LED(14)
 
-red.on()
-sleep(5)
-red.off()
-sleep(5)
-red.on()
-sleep(5)
-red.off()
-sleep(5)
+for x in range(3)
+  red.on()
+  sleep(1)
+  red.off()
+  sleep(1)
