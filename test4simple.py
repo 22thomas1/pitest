7158a1e1c2e990790c5f96af1735febe8d72f50c
22thomas1 <40281620+22thomas1@users.noreply.github.com>
2021-04-25 03:57:23 -0500

    Update test4simple.py

--- test4simple.py
+++ test4simple.py
@@ -1,9 +1,10 @@
 from  pycreate2 import Create2
 import time
+import math
 
 def sen(bot):
   data = bot.get_sensors()
-  return int(f"{data.distance:4}")
+  return int(f"{data.angle:4}")
 
 # Create a Create2.
 port = "/dev/ttyUSB0"  # where is your serial port?
@@ -15,12 +16,14 @@ bot.full()
 
 #print(bot.get_sensors())
 
+angle=90
+travelDistance = abs((angle/360)*(math.pi*233))
 
-dist = sen(bot)
-bot.drive_direct(100,100)
-while dist<200:
-  dist+=sen(bot)
-  print(dist)
+ang = sen(bot)
+bot.drive_direct(-100,100)
+while ang<travelDistance:
+  ang+=sen(bot)
+  print(ang)
 bot.drive_stop()
 
 
