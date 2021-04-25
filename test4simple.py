59e8aac745fccb0c5440b2685fc3cef2371f858a
22thomas1 <40281620+22thomas1@users.noreply.github.com>
2021-04-25 03:31:00 -0500

    Update test4simple.py

--- test4simple.py
+++ test4simple.py
@@ -1,9 +1,9 @@
 from  pycreate2 import Create2
 import time
 
-def sen(bot,id):
+def sen(bot):
   data = bot.get_sensors()
-  print(data[id-7])
+  print(data.distance)
 
 # Create a Create2.
 port = "/dev/ttyUSB0"  # where is your serial port?
@@ -14,10 +14,10 @@ bot.start()
 bot.full()
 
 print(bot.get_sensors())
-sen(bot,19)
+sen(bot)
 bot.drive_direct(100,100)
 time.sleep(1)
-sen(bot,19)
+sen(bot)
 time.sleep(1)
 bot.drive_stop()
 
