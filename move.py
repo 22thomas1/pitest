76eead53459ef8f6e21bd07d5ad0a9e69c5f86ef
22thomas1 <40281620+22thomas1@users.noreply.github.com>
2021-04-25 02:23:20 -0500

    Update move.py

--- move.py
+++ move.py
@@ -2,26 +2,7 @@ from  pycreate2 import Create2
 import time
 import math
 
-# initializations
-RLOR = initBot("/dev/ttyUSB0")
-f=open("input.txt","r")
-f.readline() #skip instruction line
-
-# global variables
-driveSpeed = 200
-turnSpeed = 200
-
-for line in f:
-  angle,distance = list(map(int,line.split(" ")))
-  turn(RLOR,angle,turnSpeed)
-  drive(RLOR,distance,driveSpeed)
-
-# cleanup
-f.close()
-RLOR.close()
-
-
-########## Functions ##########
+# functions
 def initBot(port):
   bot = Create2(port)
   bot.start()
@@ -39,6 +20,24 @@ def drive(bot,dist,speed):
   time.sleep(travelTime)
   bot.drive_stop()
 
+  
+# initializations
+RLOR = initBot("/dev/ttyUSB0")
+f=open("input.txt","r")
+f.readline() #skip instruction line
+
+# global variables
+driveSpeed = 200
+turnSpeed = 200
+
+for line in f:
+  angle,distance = list(map(int,line.split(" ")))
+  turn(RLOR,angle,turnSpeed)
+  drive(RLOR,distance,driveSpeed)
+
+# cleanup
+f.close()
+RLOR.close()
 def turn(bot,angle,speed):
   if angle==0:
     return None
