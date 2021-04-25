0621b2c68538634857a6504dc22509c974b1f5f3
22thomas1 <40281620+22thomas1@users.noreply.github.com>
2021-04-24 21:20:00 -0500

    Update move.py

--- move.py
+++ move.py
@@ -8,10 +8,18 @@ RLOR= Create2("/dev/ttyUSB0")
 RLOR.start()
 RLOR.full()
 RLOR.drive_direct(500,500)
-time.sleep(1)
-RLOR.drive_stop()
-
-
-
+time.sleep(0.5)
+RLOR.drive_direct(-500,500)
+time.sleep(0.5)
+RLOR.drive_direct(500,500)
+time.sleep(0.5)
+RLOR.drive_direct(-500,500)
+time.sleep(0.5)
+RLOR.drive_direct(500,500)
+time.sleep(0.5)
+RLOR.drive_direct(-500,500)
+time.sleep(0.5)
+RLOR.drive_direct(500,500)
+time.sleep(0.5)
 
 RLOR.close()
