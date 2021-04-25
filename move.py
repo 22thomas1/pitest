e1fe5cd8a3e2971a16bcc13f1e70d76d6ba1ab33
22thomas1 <40281620+22thomas1@users.noreply.github.com>
2021-04-25 01:49:37 -0500

    Update move.py

--- move.py
+++ move.py
@@ -2,15 +2,26 @@ from  pycreate2 import Create2
 import time
 import math
 
-# Create a Create2.
+# initializations
 RLOR = initBot("/dev/ttyUSB0")
+f=open("input.txt","r")
+f.readline() #skip instruction line
+
+# global variables
 driveSpeed = 200
 turnSpeed = 200
 
+for line in f:
+  angle,distance = list(map(int,line.split(" ")))
+  turn(RLOR,angle,turnSpeed)
+  drive(RLOR,distance,driveSpeed)
+
+# cleanup
+f.close()
+RLOR.close()
 
 
-
-
+########## Functions ##########
 def initBot(port):
   bot = Create2(port)
   bot.start()
@@ -18,6 +29,8 @@ def initBot(port):
   return bot
 
 def drive(bot,dist,speed):
+  if dist==0:
+    return None
   travelTime = dist*1000/speed
   if dist<0:  # for driving in reverse
     dist = abs(dist)
@@ -27,11 +40,11 @@ def drive(bot,dist,speed):
   bot.drive_stop()
 
 def turn(bot,angle,speed):
+  if angle==0:
+    return None
   wheelTrack = 233  # distance between two wheels (mm)
   travelDistance = (angle/360)*(math.pi*wheelTrack)  # (percentage of circle)*(circumference)
   travelTime = travelDistance/speed
   bot.drive_direct(-speed,speed)
   time.sleep(travelTime)
   bot.drive_stop()
-
-RLOR.close()
