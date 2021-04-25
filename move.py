95dcda2fc63bf44b9164ecc957a0f443bffada8a
22thomas1 <40281620+22thomas1@users.noreply.github.com>
2021-04-24 21:14:46 -0500

    Create move.py

---
+++ move.py
@@ -0,0 +1,17 @@
+from  pycreate2 import Create2
+import time
+
+# Create a Create2.
+RLOR= Create2("/dev/ttyAMA0")
+
+
+RLOR.start()
+RLOR.full()
+RLOR.drive_direct(500,500)
+sleep(1)
+RLOR.drive_stop()
+
+
+
+
+RLOR.close()
